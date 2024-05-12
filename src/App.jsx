557736b1e92529a85144956c3b9a2293539fc61c
archishmagoli <agoli6@gatech.edu>
2024-05-11 22:53:25 -0400

    api call setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cryptoList, setCryptoList] = useState([]);
+  const api_key = import.meta.env.VITE_API_KEY;
+
+  useEffect(()  => {
+    fetchCoins();
+  }, [cryptoList]);
+
+  const fetchCoins = () => {
+
+  }
 
   return (
     <>        
+      <h1>My Crypto Tracker</h1>
+      <h2>Top 100 Crypto Coins</h2>
+      <p>See the top traded coins by total volume across all markets in the last 24 hours.</p>
     </>
   )
 }
